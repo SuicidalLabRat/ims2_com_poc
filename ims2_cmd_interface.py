6bd64c94896ccc9bdddbdded16fe680bcc431b02
Marc Pernia <marc.pernia@redaptiveinc.com>
2019-11-26 15:02:12 -0800

    Removed handling of the SIGKILL signal.

--- ims2_cmd_interface.py
+++ ims2_cmd_interface.py
@@ -216,6 +216,7 @@ class GracefulKiller:
 
     def __init__(self):
         signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGKILL, self.exit_gracefully)
         signal.signal(signal.SIGTERM, self.exit_gracefully)
 
     def exit_gracefully(self, signum, frame):
